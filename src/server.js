9ab8e6cf7b2575fe24c2b28572a6dad8b8230e66
yje9802 <jidak1165@gmail.com>
2022-08-11 00:29:29 +0900

    examples of middleware
    미들웨어 연습

--- src/server.js
+++ src/server.js
@@ -5,6 +5,24 @@ const app = express();
 
 const PORT = 4000;
 
+const exampleMiddleware = (req, res, next) => {
+	console.log(`${req.method} ${req.url}`);
+	next();
+};
+const privateMiddleware = (req, res, next) => {
+	const url = req.url;
+	if (url === "/protected") {
+		// kill the connection
+		return res.send("<h1>Not Allowed</h1>");
+	}
+	console.log("Allowed");
+	next();
+};
+// 모든 라우터에 적용되는 global middleware
+// 순서가 중요
+app.use(exampleMiddleware);
+app.use(privateMiddleware);
+
 app.get("/", (req, res) => {
 	console.log("Somebody is trying to go home.");
 	// response 없이 request 종료
@@ -14,10 +32,13 @@ app.get("/", (req, res) => {
 const handleLogin = () => {
 	return res.send("Login here!");
 };
-
 app.get("/login", handleLogin);
+
+const handleProtected = (req, res) => {
+	return res.send("Welcome to the private rounge.");
+};
+app.get("/protected", handleProtected);
 
 const handleListening = () =>
 	console.log(`🍏 Server Listening on http://localhost:${PORT}`);
-
 app.listen(PORT, handleListening);
