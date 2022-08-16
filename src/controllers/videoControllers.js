43e0d0919caf0adc69ddd6569056521f6a5e28e4
yje9802 <jidak1165@gmail.com>
2022-08-17 00:24:57 +0900

    Using Pug mixins
    pug의 iteration 문법과 mixin 기능을 사용하여 비디오 블록을 보여주는 연습

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -1,5 +1,31 @@
 export const trending = (req, res) => {
-	return res.render("home", { pageTitle: "Home" });
+	const videos = [
+		{
+			title: "Hello",
+			rating: 5,
+			comments: 2,
+			createdAt: "2 minutes ago",
+			view: 59,
+			id: 1,
+		},
+		{
+			title: "Jannabi",
+			rating: 5,
+			comments: 42,
+			createdAt: "10 minutes ago",
+			view: 100,
+			id: 2,
+		},
+		{
+			title: "Paul Kim",
+			rating: 5,
+			comments: 34,
+			createdAt: "4 minutes ago",
+			view: 77,
+			id: 3,
+		},
+	];
+	return res.render("home", { pageTitle: "Home", videos });
 };
 
 export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
