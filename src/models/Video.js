82b267cb9f543efcbd109b08077cc26afda612f9
yje9802 <jidak1165@gmail.com>
2022-08-23 01:04:54 +0900

    Some edits
    :id 제한하는 정규표현식 수정
    findById로 비디오 찾기 추가

--- src/models/Video.js
+++ src/models/Video.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 // 데이터베이스 스키마 설정
 const videoSchema = new mongoose.Schema({
-	title: { type: String, required: true },
-	description: { type: String, required: true },
+	title: { type: String, required: true, trim: true, maxLength: 80 },
+	description: { type: String, required: true, trim: true, maxLength: 140 },
 	// createdAt은 필수요소이면서 default 값이 있음
 	createdAt: { type: Date, required: true, default: Date.now },
-	hashtags: [{ type: String }],
+	hashtags: [{ type: String, trim: true }],
 	meta: {
 		views: { type: Number, required: true, default: 0 },
 		rating: { type: Number, required: true, default: 0 },
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -15,9 +15,9 @@ videoRouter.route("/upload").get(getUpload).post(postUpload);
 // upload가 제일 위에 와야 함. /:id 아래 위치하면 /upload에서 upload를 id로 여기게 됨
 // :id는 숫자만 가능하도록 해야함 => 정규표현식을 활용하자 => 그러면 upload가 맨 뒤로 가도 됨
 // videoRouter.get("/:id", see);
-videoRouter.get("/:id(\\d+)", watch);
-videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+videoRouter.get("/:id([0-9a-f]{24})", watch);
+videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
 
-videoRouter.get("/:id(\\d+)/delete", deleteVideo);
+videoRouter.get("/:id([0-9a-f]{24})/delete", deleteVideo);
 
 export default videoRouter;
