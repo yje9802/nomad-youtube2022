35f9f4b3bcc3a341b75af90dd7b3092a96d12801
yje9802 <jidak1165@gmail.com>
2022-08-23 03:07:28 +0900

    Edit Video, middleware before save
    비디오 edit 화면 수정하고,
    Video.js에서 미들웨어 연습하고
    static 사용

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -1,4 +1,4 @@
-import Video from "../models/Video.js";
+import Video, { formatHashtags } from "../models/Video.js";
 
 // video 데이터를 다 받아오고 render 해주기 위해 promise 사용 (callback과 유사하나 다름)
 export const home = async (req, res) => {
@@ -9,17 +9,35 @@ export const home = async (req, res) => {
 export const watch = async (req, res) => {
 	const { id } = req.params;
 	const video = await Video.findById(id);
-	return res.render("watch", { pageTitle: video.title, video });
+	if (video) {
+		return res.render("watch", { pageTitle: video.title, video });
+	}
+	return res.render("404", { pageTitle: "Video Not Found" });
 };
 
-export const getEdit = (req, res) => {
+export const getEdit = async (req, res) => {
 	const { id } = req.params;
-	return res.render("edit", { pageTitle: `Edit` });
+	const video = await Video.findById(id);
+	if (!video) {
+		return res.render("404", { pageTitle: "Video Not Found" });
+	}
+	return res.render("edit", { pageTitle: `Edit ${video.title}`, video });
 };
-export const postEdit = (req, res) => {
+export const postEdit = async (req, res) => {
 	const { id } = req.params;
 	// form의 method를 POST로 설정하면, form에 입력된 데이터를 req.body로 전송
-	const { title } = req.body;
+	// const { title } = req.body;
+	const { title, description, hashtags } = req.body;
+	// exist 여부가 boolean으로 반환
+	const video = await Video.exists({ _id: id });
+	if (!video) {
+		return res.render("404", { pageTitle: "Video Not Found" });
+	}
+	await Video.findByIdAndUpdate(id, {
+		title,
+		description,
+		hashtags: Video.formatHashtags(hashtags),
+	});
 	return res.redirect(`/videos/${id}`);
 };
 
@@ -35,7 +53,7 @@ export const postUpload = async (req, res) => {
 		const video = new Video({
 			title: title,
 			description,
-			hashtags: hashtags.split(",").map((word) => `#${word}`),
+			hashtags: Video.formatHashtags(hashtags),
 		});
 		// video가 db에 다 저장될 때까지 기다림
 		await video.save();
--- src/models/Video.js
+++ src/models/Video.js
@@ -11,6 +11,25 @@ const videoSchema = new mongoose.Schema({
 		rating: { type: Number, required: true, default: 0 },
 	},
 });
+
+// 미들웨어는 모델 생성 전에 작성되어야 함
+
+// 이 미들웨어는 save 이벤트 이전에 적용됨
+// videoSchema.pre("save", async function () {
+// 	// 해쉬태그가 하나의 문자열 째로 리스트에 들어가기 때문에 사실상 리스트의 원소는 1개
+// 	this.hashtags = this.hashtags[0]
+// 		.split(",")
+// 		.map((word) => (word.startsWith("#") ? word : `#${word}`));
+// });
+
+// 대신 video static 사용
+// Video.formatHashtags 이렇게 사용 가능
+videoSchema.static("formatHashtags", function (hashtags) {
+	return hashtags
+		.split(",")
+		.map((word) => (word.startsWith("#") ? word : `#${word}`));
+});
+
 // 스키마 기반으로 모델 생성
 const Video = mongoose.model("Video", videoSchema);
 
