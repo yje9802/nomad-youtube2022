98b9c7242f056e1ebf5b23c55acb5d2161847909
yje9802 <jidak1165@gmail.com>
2022-08-24 02:59:15 +0900

    Updated Delete & Search page
    비디오 삭제 및 검색 페이지

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -2,7 +2,7 @@ import Video, { formatHashtags } from "../models/Video.js";
 
 // video 데이터를 다 받아오고 render 해주기 위해 promise 사용 (callback과 유사하나 다름)
 export const home = async (req, res) => {
-	const videos = await Video.find({});
+	const videos = await Video.find({}).sort({ createdAt: "desc" });
 	return res.render("home", { pageTitle: "Home", videos });
 };
 
@@ -41,7 +41,16 @@ export const postEdit = async (req, res) => {
 	return res.redirect(`/videos/${id}`);
 };
 
-export const search = (req, res) => res.send("Search");
+export const search = async (req, res) => {
+	const { keyword } = req.query;
+	if (keyword) {
+		const videos = await Video.find({
+			title: { $regex: new RegExp(keyword, "i") },
+		});
+		return res.render("search", { pageTitle: "Search", videos });
+	}
+	return res.render("search", { pageTitle: "Search" });
+};
 
 export const getUpload = (req, res) => {
 	return res.render("Upload", { pageTitle: `Upload Video` });
@@ -72,4 +81,8 @@ export const postUpload = async (req, res) => {
 	// 이렇게 해도 같은 결과
 };
 
-export const deleteVideo = (req, res) => res.send("Delete Video");
+export const deleteVideo = async (req, res) => {
+	const { id } = req.params;
+	await Video.findByIdAndDelete(id);
+	return res.redirect("/");
+};
